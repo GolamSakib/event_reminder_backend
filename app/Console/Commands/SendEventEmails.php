a43045255d2c69fb05b3de375aa386755eb6157a
GolamSakib <sakib.pust.ice@gmail.com>
2025-02-16 15:55:42 +0600

    asaklakka

--- app/Console/Commands/SendEventEmails.php
+++ app/Console/Commands/SendEventEmails.php
@@ -1,16 +1,14 @@
 <?php
-
 namespace App\Console\Commands;
 
-use App\Models\Event;
 use App\Mail\EventReminder;
+use App\Models\Event;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
 
-
 class SendEventEmails extends Command
 {
-    protected $signature = 'events:email';
+    protected $signature   = 'events:email';
     protected $description = 'Send email reminders for upcoming events';
     public function __construct()
     {
@@ -24,18 +22,19 @@ class SendEventEmails extends Command
      */
     public function handle()
     {
-        $events = Event::whereDate('startDate', now()) // Ensure the event is today
-        ->whereBetween('startDate', [now()->subMinutes(10), now()->addMinutes(5)]) // Start date is between now-10 min and now+5 min
-        ->where('is_notification_sent', 0) // Only events that haven't sent a notification
-        ->get();
-
+        $events = Event::where('startDate', '<=', now()->addMinutes(5))
+            ->where('is_notification_sent', 0)
+            ->get();
 
         foreach ($events as $event) {
             foreach ($event->participants as $participant) {
                 Mail::to($participant->participant_email)->send(new EventReminder($event));
+                $participant->is_notification_sent = 1;
+                $participant->save();
             }
             $event->is_notification_sent = 1;
             $event->save();
         }
+
     }
 }
